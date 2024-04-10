ac7f3550c0a39392a5064de22cfdf5290d4c1928
daniel <d.danielprince2003@gmail.com>
2024-04-10 22:28:33 +0530

    feat:chat ai toolkit and video ai toolkit added

---
+++ Chat AI/chat_app.py
@@ -0,0 +1,34 @@
+import os
+import replicate
+import gradio as gr
+
+os.environ["REPLICATE_API_TOKEN"] = "r8_NGoa4pvezqGTNUa2Y2kidYFr4ckcNQc30VDwU"
+
+pre_prompt = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
+
+
+def response(prompt_input):
+    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5',
+                        input={"prompt": f"{pre_prompt} {prompt_input} Assistant: ", # Prompts
+                        "temperature":0.1, "top_p":0.9, "max_length":128, "repetition_penalty":1})
+    
+    full_response = ""
+
+    for item in output:
+        full_response += item
+    return full_response
+     
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox()
+    clear = gr.ClearButton([msg, chatbot])
+
+    def respond(message, chat_history):
+        bot_message = response(message)
+        chat_history.append((message, bot_message))
+        return "", chat_history
+
+    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+    
+demo.launch()
---
+++ Video AI/video_app.py
@@ -0,0 +1,33 @@
+import replicate 
+import os
+import gradio as gr
+os.environ["REPLICATE_API_TOKEN"] = "r8_NGoa4pvezqGTNUa2Y2kidYFr4ckcNQc30VDwU"
+
+
+def display(prompt):
+    output = replicate.run(
+    "deforum/deforum_stable_diffusion:e22e77495f2fb83c34d5fae2ad8ab63c0a87b6b573b6208e1535b23b89ea66d6",
+    input={
+        "fps": 15,
+        "zoom": "0: (1.04)",
+        "angle": "0:(0)",
+        "sampler": "klms",
+        "max_frames": 100,
+        "translation_x": "0: (0)",
+        "translation_y": "0: (0)",
+        "color_coherence": "Match Frame 0 LAB",
+        "animation_prompts": prompt
+    }
+)
+    return output
+
+
+
+demo = gr.Interface(
+    fn=display,
+    inputs=[gr.components.Textbox(label='Input text')],
+    outputs=gr.components.Video(label='Generated Video'),
+    allow_flagging='never'
+)
+
+demo.launch(debug=False)
