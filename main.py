9df313f5b63a1125928a088f2ef357069c2c97a7
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-04-11 18:09:04 +0530

    feat: adding components into project

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 import gradio as gr
 from Chat_AI.chat_app import chat_demo
 from Video_AI.video_app import video_demo
+from Image_AI.image_model import image
+from Code_AI.code_model import code
+from Music_AI.music_model import music
 app = FastAPI()
 
 @app.get('/')
@@ -10,3 +13,6 @@ async def root():
 
 app1 = gr.mount_gradio_app(app, chat_demo, path='/chat')
 app2 = gr.mount_gradio_app(app, video_demo, path='/video')
+app3 = gr.mount_gradio_app(app, image, path='/image')
+app4 = gr.mount_gradio_app(app, code, path='/code_gen')
+app5 = gr.mount_gradio_app(app, music, path='/audio')
